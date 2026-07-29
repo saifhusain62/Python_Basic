9e927f3c0a9bb723d88f79c3bfc2b5e55f3664e9
Saif Husain <148823252+saifhusain62@users.noreply.github.com>
2026-07-29 13:54:16 +0600

    Datatype added

---
+++ datatype.py
@@ -0,0 +1,23 @@
+#int
+age = 34
+
+print("Age:",age)
+
+#float data type
+
+marks = 85.8
+
+print("Marks:",marks)
+
+complex_number = 10 + 20j
+
+print("Complex Number:",complex_number)
+
+#string data type
+name = "Saif Husain"
+
+print("Name:",name)
+
+#list data type
+fruits = ["Apple","Banana","Mango"]
+print("Fruits:",fruits)
