e1aaf678aad301b3f4a08b0a1660af0524b1328a
Saif Husain <148823252+saifhusain62@users.noreply.github.com>
2026-08-07 11:09:31 +0600

    type check

---
+++ check.py
@@ -0,0 +1,14 @@
+a = 10 
+b = 20.5
+c = 1+2j 
+d = "hello"
+e = ['apple','banana']
+f = (1,2,3)
+g = range(1,10,2)
+h = {'name': 'alice', 'age': 21}
+i = frozenset([1,2,3])
+k = True
+l = None
+
+check = type(k)
+print(check)
